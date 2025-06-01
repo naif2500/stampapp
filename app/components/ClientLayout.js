3ec65088b148dae30e69e7f59833d030b2689c56
naif2500 <94239881+naif2500@users.noreply.github.com>
2025-06-01 22:38:38 +0200

    fixed use client issue

---
+++ app/components/ClientLayout.js
@@ -0,0 +1,16 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import Navbar from './Navbar'
+
+export default function ClientLayout({ children }) {
+  const pathname = usePathname()
+  const isHomePage = pathname === '/'
+
+  return (
+    <>
+      {!isHomePage && <Navbar />}
+      <main>{children}</main>
+    </>
+  )
+}
--- app/layout.js
+++ app/layout.js
@@ -1,40 +1,30 @@
-"use client";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Navbar from "./components/Navbar";
-import { usePathname } from 'next/navigation'; // Use usePathname from next/navigation
+import './globals.css'
+import { Geist, Geist_Mono } from 'next/font/google'
+import ClientLayout from './components/ClientLayout'
 
 export const metadata = {
   title: 'Stampify',
   description: 'A modern PWA built with Next.js',
   manifest: '/manifest.json',
-  themeColor: '#6774CA', 
+  themeColor: '#6774CA',
 }
 
-
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname(); // Get the current pathname
-  const isHomePage = pathname === "/"; // Check if it's the homepage
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {!isHomePage && <Navbar />}
-        <main  >{children}</main>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
-  );
+  )
 }
